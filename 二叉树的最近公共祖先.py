797c7de3fccbe74c712877ca762b2f434e6867eb
phy <penghongyang@17jingsai.com>
2019-12-19 17:01:32 +0800

    +

---
+++ 二叉树的最近公共祖先.py
@@ -0,0 +1,82 @@
+'''
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+示例 1:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+示例 2:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出: 5
+解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ 
+
+说明:
+
+所有节点的值都是唯一的。
+p、q 为不同节点且均存在于给定的二叉树中。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# 递归
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        self.r = TreeNode(0)
+
+        def is_father(node):
+            if not node:
+                return False
+            left = is_father(node.left)
+            right = is_father(node.right)
+            mid = node == p or node == q
+            if mid + left + right >= 2:
+                self.r = node
+            return mid or left or right
+        is_father(root)
+        return self.r
+
+
+# 父指针迭代
+class Solution2:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        fathers = {root: None}
+        stack = [root]
+
+        while p not in fathers or q not in fathers:
+            node = stack.pop()
+            if node.left:
+                fathers[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                fathers[node.right] = node
+                stack.append(node.right)
+        cache = set()
+        while p:
+            cache.add(p)
+            p = fathers[p]
+        while q not in cache:
+            q = fathers[q]
+        return q
+
+
+
+
+
--- 链表排序.py
+++ 链表排序.py
@@ -1,5 +1,5 @@
 '''
-在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
 
 示例 1:
 
@@ -9,6 +9,10 @@
 
 输入: -1->5->3->4->0
 输出: -1->0->3->4->5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
 
@@ -17,9 +21,8 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 # 一、归并排序（递归）
-
-
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
         if not head or not head.next:
@@ -42,8 +45,39 @@ class Solution:
         h.next = left if left else right
         return res.next
 
+
 # 二、归并排序（从底至顶直接合并）
-
-
-
-
+class Solution2:
+    def sortList(self, head: ListNode) -> ListNode:
+        h, length, step = head, 0, 1
+        while h:
+            h, length = h.next, length + 1
+        res = ListNode(0)
+        res.next = head
+        # merge the list in different step.
+        while step < length:
+            pre, h = res, res.next
+            while h:
+                # get the two merge head `h1`, `h2`
+                h1, i = h, step
+                while i and h:
+                    h, i = h.next, i - 1
+                if i:
+                    break  # no need to merge because the `h2` is None.
+                h2, i = h, step
+                while i and h:
+                    h, i = h.next, i - 1
+                c1, c2 = step, step - i  # the `c2`: length of `h2` can be small than the `step`.
+                # merge the `h1` and `h2`.
+                while c1 and c2:
+                    if h1.val < h2.val:
+                        pre.next, h1, c1 = h1, h1.next, c1 - 1
+                    else:
+                        pre.next, h2, c2 = h2, h2.next, c2 - 1
+                    pre = pre.next
+                pre.next = h1 if c1 else h2
+                while c1 > 0 or c2 > 0:
+                    pre, c1, c2 = pre.next, c1 - 1, c2 - 1
+                pre.next = h
+            step *= 2
+        return res.next
