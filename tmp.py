c83682ddf7f8886b4880b0133878b140b3be1c7f
phy <penghongyang@17jingsai.com>
2019-12-20 08:41:45 +0800

    +

--- tmp.py
+++ tmp.py
@@ -1,34 +1,4 @@
-'''
-给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
-
-说明：
-你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
-
-示例 1:
-
-输入: root = [3,1,4,null,2], k = 1
-   3
-  / \
- 1   4
-  \
-   2
-输出: 1
-示例 2:
-
-输入: root = [5,3,6,2,4,null,null,1], k = 3
-       5
-      / \
-     3   6
-    / \
-   2   4
-  /
- 1
-输出: 3
-进阶：
-如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
-'''
-
-
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -37,21 +7,32 @@ class TreeNode:
 
 
 class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        stack = []
-        r = []
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        p_father, q_father = None, None
 
-        while root or stack:
-            if root:
-                stack.append(root)
-                root = root.left
+        def get_node(cur, father_list):
+            if cur == p:
+                p_father = father_list
+            elif cur == q:
+                q_father = father_list
+            if p_father and q_father:
+                return
             else:
-                root = stack.pop()
-                r.append(root.val)
-                root = root.right
-                if len(r) >= k:
-                    break
-        return r[k]
+                if cur.left:
+                    cur_list = father_list + [cur]
+                    get_node(cur.left, cur_list)
+                if cur.right:
+                    cur_list = father_list + [cur]
+                    get_node(cur.right, cur_list)
+        get_node(root, [])
 
-        pass
+        i = 0
+        last = None
+        while True:
+            if p_father[i] == q_father[i]:
+                last = p_father[i]
+            else:
+                break
+            i += 1
+        return last
 
