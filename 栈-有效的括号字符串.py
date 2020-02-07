4c5cc22e7066f4341c9c98c7f9c52dd3eb1e60db
phy <penghongyang@17jingsai.com>
2020-02-07 16:20:35 +0800

    +

---
+++ 栈-有效的括号字符串.py
@@ -0,0 +1,55 @@
+'''
+给定一个只包含三种字符的字符串：（ ，） 和 *，写一个函数来检验这个字符串是否为有效字符串。有效字符串具有如下规则：
+
+任何左括号 ( 必须有相应的右括号 )。
+任何右括号 ) 必须有相应的左括号 ( 。
+左括号 ( 必须在对应的右括号之前 )。
+* 可以被视为单个右括号 ) ，或单个左括号 ( ，或一个空字符串。
+一个空字符串也被视为有效字符串。
+示例 1:
+
+输入: "()"
+输出: True
+示例 2:
+
+输入: "(*)"
+输出: True
+示例 3:
+
+输入: "(*))"
+输出: True
+注意:
+
+字符串大小将在 [1，100] 范围内。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parenthesis-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+        for i, w in enumerate(s):
+            if w == '(':
+                left.append(i)
+            elif w == '*':
+                star.append(i)
+            elif w == ')':
+                if len(left) > 0:
+                    left.pop()
+                elif len(star) > 0:
+                    star.pop()
+                else:
+                    return False
+        while left and star:
+            left_back = left.pop()
+            star_back = star.pop()
+            if star_back < left_back:
+                return False
+        if left:
+            return False
+        return True
+
