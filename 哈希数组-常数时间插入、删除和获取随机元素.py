b684469808a346818c50629dfa5bb5c0fcbf0a11
phy <penghongyang@17jingsai.com>
2020-03-03 15:36:00 +0800

    +

---
+++ 哈希数组-常数时间插入、删除和获取随机元素.py
@@ -0,0 +1,82 @@
+'''
+设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+
+insert(val)：当元素 val 不存在时，向集合中插入该项。
+remove(val)：元素 val 存在时，从集合中移除该项。
+getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+示例 :
+
+// 初始化一个空的集合。
+RandomizedSet randomSet = new RandomizedSet();
+
+// 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+randomSet.insert(1);
+
+// 返回 false ，表示集合中不存在 2 。
+randomSet.remove(2);
+
+// 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+randomSet.insert(2);
+
+// getRandom 应随机返回 1 或 2 。
+randomSet.getRandom();
+
+// 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+randomSet.remove(1);
+
+// 2 已在集合中，所以返回 false 。
+randomSet.insert(2);
+
+// 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+randomSet.getRandom();
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/insert-delete-getrandom-o1
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.d = {}
+        self.l = []
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.d:
+            return False
+        self.l.append(val)
+        self.d[val] = len(self.l) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.d:
+            return False
+        idx = self.d.pop(val)
+        if idx != len(self.l) - 1:
+            self.d[self.l[-1]] = idx
+            self.l[idx], self.l[-1] = self.l[-1], self.l[idx]
+        self.l.pop()
+        return True
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.l)
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
