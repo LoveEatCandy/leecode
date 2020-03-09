2e2766d9a367efdfaa7f28dbb3cfaf70f5ce8d5b
phy <penghongyang@17jingsai.com>
2020-03-09 13:13:30 +0800

    +

---
+++ 动态规划-最大正方形.py
@@ -0,0 +1,39 @@
+'''
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximal-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class Solution:
+    def maximalSquare(self, matrix: 'List[List[str]]') -> int:
+        if not matrix:
+            return 0
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        large = 0
+        for a in range(m):
+            for b in range(n):
+                if matrix[a][b] == '1':
+                    dp[a][b] = min(dp[a - 1][b - 1], dp[a - 1][b], dp[a][b - 1]) + 1
+                large = max(large, dp[a][b])
+        return large ** 2
+
+
+'''
+以dp[i][j]为方形右下角点
+'''
---
+++ 栈-栈的最小值.py
@@ -0,0 +1,57 @@
+'''
+请设计一个栈，除了常规栈支持的pop与push函数以外，还支持min函数，该函数返回栈元素中的最小值。执行push、pop和min操作的时间复杂度必须为O(1)。
+
+
+示例：
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = None
+
+    def push(self, x: int) -> None:
+        if self.min is None:
+            self.min = x
+        else:
+            self.min = min(self.min, x)
+        self.stack.append({"val": x, "min": self.min})
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.stack:
+            self.min = self.stack[-1]['min']
+        else:
+            self.min = None
+
+    def top(self) -> int:
+        assert self.stack
+        return self.stack[-1]['val']
+
+    def getMin(self) -> int:
+        return self.min
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
