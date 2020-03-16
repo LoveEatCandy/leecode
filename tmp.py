04105c2e22301e88be231c5ef4da237c638c95fe
phy <654879998@qq.com>
2020-03-16 17:47:19 +0800

    +

--- tmp.py
+++ tmp.py
@@ -1,18 +1,57 @@
 class Solution:
-    def numberOfPatterns(self, m: int, n: int) -> int:
-        mode = list(range(1, 10))
-        count = 1
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        if not matrix:
+            return matrix
+        if len(matrix) == 1:
+            r = []
+            cur_l = [matrix[0][0]]
+            for i in range(matrix[0][1:]):
+                if i > cur_l[-1]:
+                    cur_l.append(i)
+                else:
+                    r = cur_l if len(cur_l) > len(r) else r
+                    cur_l = [i]
+            return r
+        l_j = len(matrix)
+        l_i = len(matrix[0])
 
-        def do(cur, others):
-            nonlocal count
-            if m <= len(cur) <= n:
-                count += 1
-            for i, w in enumerate(others):
-                do(cur+[w], others[:i]+others[i+1:])
+        dp = [[0] * l_i for _ in range(l_j)]
+        r = 1
+        for j in range(l_j):
+            for i in range(l_i):
+                cur_count = 1
+                if j > 0:
+                    if matrix[j - 1][i] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j - 1][i] + 1)
+                if i > 0:
+                    if matrix[j][i - 1] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j][i - 1] + 1)
+                if j < l_j - 1:
+                    if matrix[j + 1][i] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j + 1][i] + 1)
+                if i < l_i - 1:
+                    if matrix[j][i + 1] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j][i + 1] + 1)
+                r = max(r, cur_count)
+                dp[j][i] = cur_count
 
-        do([], mode)
-        return count
+        dp = [[0] * l_i for _ in range(l_j)]
+        for j in range(l_j - 1, -1, -1):
+            for i in range(l_i - 1, -1, -1):
+                cur_count = 1
+                if j > 0:
+                    if matrix[j - 1][i] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j - 1][i] + 1)
+                if i > 0:
+                    if matrix[j][i - 1] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j][i - 1] + 1)
+                if j < l_j - 1:
+                    if matrix[j + 1][i] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j + 1][i] + 1)
+                if i < l_i - 1:
+                    if matrix[j][i + 1] < matrix[j][i]:
+                        cur_count = max(cur_count, dp[j][i + 1] + 1)
+                r = max(r, cur_count)
+                dp[j][i] = cur_count
 
-
-
-
+        return r
---
+++ 动态规划DFS-矩阵中的最长递增路径.py
@@ -0,0 +1,85 @@
+'''
+给定一个整数矩阵，找出最长递增路径的长度。
+
+对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+
+示例 1:
+
+输入: nums =
+[
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+]
+输出: 4
+解释: 最长递增路径为 [1, 2, 6, 9]。
+示例 2:
+
+输入: nums =
+[
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+]
+输出: 4
+解释: 最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+from functools import reduce
+
+
+# DP
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        all_nums = []
+        if matrix == []:
+            return 0
+        if matrix == [[]]:
+            return 0
+        width = len(matrix[0])
+        height = len(matrix)
+        for y in range(height):
+            for x in range(width):
+                all_nums.append((y, x, matrix[y][x]))
+        matrix.append([0] * width)
+        for row in matrix:
+            row.append(0)
+        saving_matrix = [[1 for _ in range(width)] for _ in range(height)]
+        all_nums = sorted(all_nums, key=lambda x: x[2], reverse=True)
+
+        while all_nums:
+            y, x, num = all_nums.pop()
+            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                ny = y + dy
+                nx = x + dx
+                if matrix[ny][nx] > num:
+                    saving_matrix[ny][nx] = max(saving_matrix[ny][nx], saving_matrix[y][x] + 1)
+        return max(reduce(lambda a, b: a + b, saving_matrix))
+
+
+# DFS
+class Solution2:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        if not matrix or not matrix[0]: return 0
+
+        row = len(matrix)
+        col = len(matrix[0])
+        lookup = [[0] * col for _ in range(row)]
+
+        def dfs(i, j):
+            if lookup[i][j] != 0:
+                return lookup[i][j]
+            res = 1
+            for x, y in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                tmp_i = x + i
+                tmp_j = y + j
+                if 0 <= tmp_i < row and 0 <= tmp_j < col and matrix[tmp_i][tmp_j] > matrix[i][j]:
+                    res = max(res, 1 + dfs(tmp_i, tmp_j))
+            lookup[i][j] = max(res, lookup[i][j])
+            return lookup[i][j]
+
+        return max(dfs(i, j) for i in range(row) for j in range(col))
