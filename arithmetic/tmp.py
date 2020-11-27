fec5090a8b34f3de9b4fc30e2dd4c906310e8d2e
phy <penghongyang@17jingsai.com>
2020-11-27 11:39:53 +0800

    +

--- arithmetic/tmp.py
+++ arithmetic/tmp.py
@@ -2,17 +2,19 @@ from typing import List
 
 
 class Solution:
-    def permutation(self, s: str) -> List[str]:
-        if not s:
-            return []
-        r = set()
+    def breakfastNumber(self, staple: List[int], drinks: List[int], x: int) -> int:
+        less_item_num = [0] * x
+        for s in staple:
+            if s < x:
+                less_item_num[s] += 1
+        accu = 0
+        for i in range(1, x):
+            accu += less_item_num[i]
+            less_item_num[i] = accu
+        res = 0
+        for d in drinks:
+            if d < x:
+                res += less_item_num[x - d]
+        return res % (10 ** 9 + 7)
 
-        def do(tmp, other):
-            if not other:
-                r.add(tmp)
-            else:
-                for i, ss in enumerate(other):
-                    do(tmp+ss, other[:i-1]+other[i+1:])
-        do('', s)
-        return list(r)
 
---
+++ arithmetic/堆-数据流中的中位数.py
@@ -0,0 +1,89 @@
+'''
+数据流中的中位数
+如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+
+例如，
+
+[2,3,4] 的中位数是 3
+
+[2,3] 的中位数是 (2 + 3) / 2 = 2.5
+
+设计一个支持以下两种操作的数据结构：
+
+void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+double findMedian() - 返回目前所有元素的中位数。
+示例 1：
+
+输入：
+["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]
+[[],[1],[2],[],[3],[]]
+输出：[null,null,null,1.50000,null,2.00000]
+示例 2：
+
+输入：
+["MedianFinder","addNum","findMedian","addNum","findMedian"]
+[[],[2],[],[3],[]]
+输出：[null,null,2.00000,null,2.50000]
+
+
+限制：
+
+最多会对 addNum、findMedian 进行 50000 次调用。
+'''
+import heapq
+import bisect
+
+
+# 二分插入
+class MedianFinder1:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.store = []
+
+    def addNum(self, num: int) -> None:
+        if not self.store:
+            self.store.append(num)
+        else:
+            bisect.insort_left(self.store, num)  # 插入
+
+    def findMedian(self) -> float:
+        n = len(self.store)
+        if n & 1 == 1:  # n是奇数
+            return self.store[n // 2]
+        else:
+            return (self.store[n // 2] + self.store[n // 2 - 1]) / 2
+
+
+# 堆
+class MedianFinder2:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        # 初始化大顶堆和小顶堆
+        self.max_heap = []
+        self.min_heap = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.max_heap) == len(self.min_heap):  # 先加到大顶堆，再把大堆顶元素加到小顶堆
+            heapq.heappush(self.min_heap, -heapq.heappushpop(self.max_heap, -num))
+        else:  # 先加到小顶堆，再把小堆顶元素加到大顶堆
+            heapq.heappush(self.max_heap, -heapq.heappushpop(self.min_heap, num))
+
+    def findMedian(self) -> float:
+        if len(self.min_heap) == len(self.max_heap):
+            return (-self.max_heap[0] + self.min_heap[0]) / 2
+        else:
+            return self.min_heap[0]
+
+
+'''
+作者：z1m
+链接：https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/you-xian-dui-lie-by-z1m/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
