c860c988756931910b9a0df8e3bbc57932f39a81
phy <penghongyang@17jingsai.com>
2021-01-18 11:30:57 +0800

    +

--- arithmetic/tmp.py
+++ arithmetic/tmp.py
@@ -2,19 +2,19 @@ from typing import List
 
 
 class Solution:
-    def breakfastNumber(self, staple: List[int], drinks: List[int], x: int) -> int:
-        less_item_num = [0] * x
-        for s in staple:
-            if s < x:
-                less_item_num[s] += 1
-        accu = 0
-        for i in range(1, x):
-            accu += less_item_num[i]
-            less_item_num[i] = accu
-        res = 0
-        for d in drinks:
-            if d < x:
-                res += less_item_num[x - d]
-        return res % (10 ** 9 + 7)
+    def maxSumWithLeftRight(self, A, L, R):
+        max_L = sum(A[:L])
+        res = max_L
+        for i in range(L, len(A)-R+1):
+            max_L = max(max_L, sum(A[i-L:i]))
+            res = max(res, max_L + sum(A[i:i+R]))
+            print(A[i-L:i], A[i:i+R], max_L, res)
+        return res
+
+    def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
+        return max(self.maxSumWithLeftRight(A, L, M), self.maxSumWithLeftRight(A, M, L))
 
 
+print(Solution().maxSumTwoNoOverlap([1, 0, 3], 1, 2))
+
+20
